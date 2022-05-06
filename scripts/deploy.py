bba84d2da9db37a64e6ec1c91a5cd2265e89fb9f
peterrafaeldelgado <77725497+peterdelgado-droid@users.noreply.github.com>
2022-05-06 11:22:40 +0200

    helpful scripts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,1 +1,9 @@
-def deploy_fund_me()
+from brownie import Fund
+from scripts.helpful_scripts import get_account
+
+
+def deploy_fund_me():
+    account = get_account()
+
+def main():
+    deploy_fund_me()
